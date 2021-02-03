4a869f234bb795f38538aa674d923dcc494aec35
Mavchi <aleksi.khlebnikov@gmail.com>
2021-02-03 18:00:06 +0200

    fixed minor issue with sending appData

--- back/controllers/appData.js
+++ back/controllers/appData.js
@@ -28,7 +28,7 @@ appData_test = {
         },
         {
             name: "Popular",
-            workout_ids: ["601a5716f10018ca6c39dba9"],
+            workout_ids: ["6017fd12ac151833101a62a3"],
             filter: ["muscle"],
             workouts: [],
         },
@@ -43,14 +43,14 @@ const Workout = require('../models/workout')
 
 appDataRouter.get('/', async (request, response) => {
     const app_data = appData_test 
-    const workouts = []
     for(let i=0; i < app_data.workout_menus.length; i++){
+        const workouts = []
         for(let j=0; j < app_data.workout_menus[i].workout_ids.length; j++){
             const workout = await Workout.findById(app_data.workout_menus[i].workout_ids[j])
             workouts.push(workout.toJSON())
         }
+        app_data.workout_menus[i].workouts = workouts
     }
-    app_data.workout_menus[i].workouts = workouts
     //console.log(app_data)
     response.json(app_data)
 })
